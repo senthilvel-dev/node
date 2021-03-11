780e69b46d2fbde8b9d75eb87cd004ea0e1b5cdd
senthilvel-dev <senthilveldon@gmail.com>
2021-03-11 10:19:59 +0530

    commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const client = new Client(connectionstring);
 const Express = require('express');
 const app = new Express();
 var bodyParser = require('body-parser');
+const employees_det = require('./models/employees_det');
+const { response } = require('express');
 var urlencode = bodyParser.json({extended:true})
 client.connect(function(err){
     if(err){
@@ -13,10 +15,45 @@ client.connect(function(err){
     console.log("connected")
 })
 app.post('/parse',urlencode,function(req,res){
-    console.log(req.body.firstname)
-    res.send(req.body.firstname)
+return employees_det.create({
+designation: req.body.position,
+a_course: req.body.training,
+b_ac_no: req.body.bank_ac_no,
+b_group: req.body.blood_group,
+bank_name: req.body.bank_name,
+c_address: req.body.present_address,
+c_no: req.body.contact_no,
+dob: req.body.dob,
+e_contact: req.body.e_contact,
+emp_id: req.body.crayond_id,
+emp_name: req.body.name,
+exp_det: req.body.experience,
+f_members: req.body.family_members,
+f_name: req.body.father_name,
+hsc: req.body.hsc,
+l_known: req.body.language_known,
+m_no: req.body.mobile_no,
+mar_stat: req.body.maritial_status,
+nominee: req.body.nominee,
+p_address: req.body.permenant_address,
+p_email: req.body.personal_email,
+pg: req.body.pg,
+s_s_l_c: req.body.sslc,
+ug: req.body.ug,
+ifsc_code: req.body.ifsc_code,
+doj: req.body.doj,
+c_person_1: req.body.contact_person_1,
+c_person_2: req.body.contact_person_2,
+id_mark: req.body.id_mark,
+}).then(function(employees_det){
+    if(employees_det){
+        res.send(employees_det)
+    }
+    else{
+        res.status(400).send('error inserting a record');
+    }
+})  
 })
-
 var server = app.listen(8000,function ()
 {
     var host = server.address().address;
--- models/employees_det.js
+++ models/employees_det.js
@@ -1,71 +1,71 @@
 /* jshint indent: 2 */
 module.exports = function (sequelize, DataTypes) {
-  const emp = sequelize.define(
-    "emp",
+  const employees_det = sequelize.define(
+    "employees_det",
     {
       designation: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       b_ac_no: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       b_group: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       bank_name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       c_address: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       emp_id: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       emp_name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       f_name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       p_address: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       p_email: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
-      ifsc_code: { type: DataTypes.STRING, allowNull: false },
-      dob: { type: DataTypes.STRING, allowNull: false },
-      doj: { type: DataTypes.STRING, allowNull: false },
-      c_no: { type: DataTypes.INTEGER, allowNull: false },
-      m_no: { type: DataTypes.INTEGER, allowNull: false },
-      hsc: { type: DataTypes.JSON, allowNull: false },
-      s_s_l_c: { type: DataTypes.JSON, allowNull: false },
-      a_course: { type: DataTypes.JSON, allowNull: false },
-      e_contact: { type: DataTypes.JSON, allowNull: false },
-      exp_det: { type: DataTypes.JSON, allowNull: false },
-      f_members: { type: DataTypes.JSON, allowNull: false },
-      l_known: { type: DataTypes.JSON, allowNull: false },
-      nominee: { type: DataTypes.JSON, allowNull: false },
-      pg: { type: DataTypes.JSON, allowNull: false },
-      ug: { type: DataTypes.JSON, allowNull: false },
-      c_person_1: { type: DataTypes.JSON, allowNull: false },
-      c_person_2: { type: DataTypes.JSON, allowNull: false },
-      id_mark: { type: DataTypes.JSON, allowNull: false },
-      s_no: { type: DataTypes.NUMBER, allowNull: false },
+      ifsc_code: { type: DataTypes.STRING, allowNull: true },
+      dob: { type: DataTypes.STRING, allowNull: true },
+      doj: { type: DataTypes.STRING, allowNull: true },
+      c_no: { type: DataTypes.INTEGER, allowNull: true },
+      m_no: { type: DataTypes.INTEGER, allowNull: true },
+      hsc: { type: DataTypes.JSON, allowNull: true },
+      s_s_l_c: { type: DataTypes.JSON, allowNull: true },
+      a_course: { type: DataTypes.JSON, allowNull: true },
+      e_contact: { type: DataTypes.JSON, allowNull: true },
+      exp_det: { type: DataTypes.JSON, allowNull: true },
+      f_members: { type: DataTypes.JSON, allowNull: true },
+      l_known: { type: DataTypes.JSON, allowNull: true },
+      nominee: { type: DataTypes.JSON, allowNull: true },
+      pg: { type: DataTypes.JSON, allowNull: true },
+      ug: { type: DataTypes.JSON, allowNull: true },
+      c_person_1: { type: DataTypes.JSON, allowNull: true },
+      c_person_2: { type: DataTypes.JSON, allowNull: true },
+      id_mark: { type: DataTypes.JSON, allowNull: true },
+      s_no: { type: DataTypes.NUMBER, allowNull: true },
     },
     {
       tableName: "employees_det",
     }
   );
-  return emp;
+  return employees_det;
 };
